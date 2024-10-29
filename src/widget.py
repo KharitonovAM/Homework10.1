a7259af4521970aa1938cc41341b9d3caa791f90
aleksandr <kharitonov_am@bk.ru>
2024-10-29 22:19:12 +0300

    add widget

---
+++ src/widget.py
@@ -0,0 +1,15 @@
+from src.masks import get_mask_account as account
+from src.masks import get_mask_card_number as card
+
+
+def mask_account_card(data_of_card: str) -> str:
+    """Принимает номер карты или номер чсета и возвражает его под маской"""
+    if data_of_card.split()[0].lower() == "счет":
+        return data_of_card.split()[0] + account(data_of_card.split()[1])
+    else:
+        return data_of_card.split()[0] + card(data_of_card.split()[1])
+
+
+def get_date(string_whith_data: str) -> str:
+    """Изменяет формат даты"""
+    return ".".join(string_whith_data.split("T")[0].split("-")[::-1])
