c0f99a00372f415dcbffd98b450b7d1baa6d5a5d
aleksandr <kharitonov_am@bk.ru>
2024-11-06 23:54:46 +0300

    flake8 it is ok for processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,9 @@
-def filter_by_state(list_of_dict: dict, state: str = 'EXECUTED')->list:
+def filter_by_state(list_of_dict: dict, state: str = 'EXECUTED') -> list:
     '''Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
 state соответствует указанному значению'''
-    return [x for x in list_of_dict if x['state']==state]
+    return [x for x in list_of_dict if x['state'] == state]
 
 
-def sort_by_date(list_of_dict: dict, date_reverse: bool = False) ->list:
+def sort_by_date(list_of_dict: dict, date_reverse: bool = False) -> list:
     '''Возвращает список отсортированныц по параметру date, принимает необязательный параметр date_reverse'''
     return sorted(list_of_dict, key=lambda x: x['date'], reverse=date_reverse)
-
