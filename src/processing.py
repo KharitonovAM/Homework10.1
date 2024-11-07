fb3e4024135ae426e3905afc7251022a336a2507
aleksandr <kharitonov_am@bk.ru>
2024-11-07 20:06:06 +0300

    change typing in processing

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,12 @@
-def filter_by_state(list_of_dict: dict, state: str = "EXECUTED") -> list:
+from typing import Any
+
+
+def filter_by_state(list_of_dict: list[dict[Any]], state: str = "EXECUTED") -> list[dict[Any]]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению"""
-    return [x for x in list_of_dict if x["state"] == state]
+    return [item for item in list_of_dict if item["state"] == state]
 
 
-def sort_by_date(list_of_dict: dict, date_reverse: bool = False) -> list:
+def sort_by_date(list_of_dict: list[dict[Any]], date_reverse: bool = False) -> list[dict[Any]]:
     """Возвращает список отсортированныц по параметру date, принимает необязательный параметр date_reverse"""
-    return sorted(list_of_dict, key=lambda x: x["date"], reverse=date_reverse)
+    return sorted(list_of_dict, key=lambda item: item["date"], reverse=date_reverse)
