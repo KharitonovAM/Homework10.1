1b9e1d7108ed191462a8b9c6ec8ea65bcd2ecbe1
aleksandr <kharitonov_am@bk.ru>
2024-11-13 20:31:40 +0300

    change black for all

--- test/conftest.py
+++ test/conftest.py
@@ -4,5 +4,4 @@ import random
 
 @pytest.fixture
 def make_rand_card_number():
-    return random.randint(999999999999999,9999999999999999)
-
+    return random.randint(999999999999999, 9999999999999999)
--- test/test_widget.py
+++ test/test_widget.py
@@ -3,18 +3,31 @@ import pytest
 
 from src.widget import get_date, mask_account_card
 
-@pytest.mark.parametrize('date, result', [('счет 123123123111', 'счет **3111'), ('карта 1234567890123456','карта 1234 56** **** 3456')])
+
+@pytest.mark.parametrize(
+    "date, result",
+    [
+        ("счет 123123123111", "счет **3111"),
+        ("карта 1234567890123456", "карта 1234 56** **** 3456"),
+    ],
+)
 def test_mask_account_card(date, result):
     mask_account_card(date) == result
 
 
 def test_mask_account_card1():
-    assert mask_account_card('счет 56455455656565465465465654') == 'счет **5654'
+    assert mask_account_card("счет 56455455656565465465465654") == "счет **5654"
 
     with pytest.raises(IndexError):
-        assert mask_account_card('счет56455455656565465465465654')
+        assert mask_account_card("счет56455455656565465465465654")
 
 
-@pytest.mark.parametrize("data,result", [('2024-03-11T02:26:18.671407', '11.03.2024'),('2024-10-18T02:26:18.671407', '18.10.2024')])
+@pytest.mark.parametrize(
+    "data,result",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("2024-10-18T02:26:18.671407", "18.10.2024"),
+    ],
+)
 def test_get_date(data, result):
-    get_date(data)==result
+    get_date(data) == result
