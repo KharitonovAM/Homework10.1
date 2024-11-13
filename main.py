b282f457ad025a26783856698ee97def3f650b95
aleksandr <kharitonov_am@bk.ru>
2024-11-13 20:28:37 +0300

    make cover by  test_processing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.widget import mask_account_card
 from src.widget import get_date
+from src.processing import sort_by_date
 
 
 # Press the green button in the gutter to run the script.
@@ -8,5 +9,9 @@ if __name__ == '__main__':
     print(mask_account_card('карта 123232121323232'))
     print(mask_account_card('счет 123232121323232'))
     print(get_date('11.08/2024'))
+
+    print(sort_by_date(
+        [{}]
+    ))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
