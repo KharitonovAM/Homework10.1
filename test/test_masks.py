16a46c4dc09361eb827f8b518395591f0bcd7613
aleksandr <kharitonov_am@bk.ru>
2024-11-13 17:25:23 +0300

    make tests for masks

---
+++ test/test_masks.py
@@ -0,0 +1,10 @@
+import pytest
+from src.masks import get_mask_account, get_mask_card_number
+
+@pytest.mark.parametrize("data, result", [(13123,'**3123'), ('','**')])
+def test_get_mask_account(data, result):
+    assert get_mask_account(data) == result
+
+@pytest.mark.parametrize("data, result", [(1234567890123456,'1234 56** **** 3456')])
+def test_get_mask_card_number(data, result):
+    assert get_mask_card_number(data) == result
