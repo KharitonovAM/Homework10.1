ec1b5a927223dee4ec181ebef9f0ed44c280cedf
aleksandr <kharitonov_am@bk.ru>
2024-11-20 17:11:53 +0300

    make 10st test by filter_by_currency succsesfull

--- test/test_generators.py
+++ test/test_generators.py
@@ -3,10 +3,8 @@ from src.generators import filter_by_currency, transaction_descriptions, card_nu
 
 
 def test_generators_filter(make_generator_filter_list):
-    temp_filter = card_number_generator(make_generator_filter_list)
+    temp_filter = filter_by_currency(make_generator_filter_list,'USD')
     test_list = []
     for i in range(2):
         test_list.append(next(temp_filter))
     assert test_list == make_generator_filter_list
-
-
