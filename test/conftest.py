e82d40d5d4ddbda409cad2a50c5242dcb1a7d1aa
aleksandr <kharitonov_am@bk.ru>
2024-11-20 17:48:46 +0300

    add function test of transaction_descriptions

--- test/conftest.py
+++ test/conftest.py
@@ -41,3 +41,66 @@ def make_generator_filter_list():
               "to": "Счет 75651667383060284188"
        }
 ]
+
+def make_long_generator_filter_list():
+    return [{
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      },
+      {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "USD",
+                      "code": "USD"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       },
+       {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "BYN",
+                    "code": "BYN"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+       },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "RUS",
+                    "code": "RUS"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }
+]
--- test/test_generators.py
+++ test/test_generators.py
@@ -15,3 +15,17 @@ def test_generatore_erise(make_generator_filter_list):
     with pytest.raises(StopIteration):
         for i in range(4):
             next(temp_filter)
+
+def test_generators_filter_with_four(make_generator_filter_list, make_long_generator_filter_list):
+    temp_filter = filter_by_currency(make_long_generator_filter_list, 'USD')
+    test_list = []
+    for i in range(2):
+        test_list.append(next(temp_filter))
+    assert test_list == make_generator_filter_list
+
+def test_transaction_base_functions(make_generator_filter_list):
+    test_transaction_descriptions = transaction_descriptions(make_generator_filter_list)
+    test_list = []
+    for i in range(2):
+        test_list.append(next(test_transaction_descriptions))
+    assert test_list == ["Перевод организации", "Перевод со счета на счет"]
