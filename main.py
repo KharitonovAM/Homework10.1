46c2d274f58e51fe2f431928af4e684f15ba5433
aleksandr <kharitonov_am@bk.ru>
2024-11-20 16:48:55 +0300

    make func's names with passes in generators.py

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from src.widget import mask_account_card
 from src.widget import get_date
 from src.processing import sort_by_date
+from src.generators import filter_by_currency
 
+test =  [{
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      },
+      {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "USD",
+                      "code": "USD"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       }
+]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -9,6 +41,15 @@ if __name__ == '__main__':
     print(mask_account_card('карта 123232121323232'))
     print(mask_account_card('счет 123232121323232'))
     print(get_date('11.08/2024'))
+
+
+
+    q = filter_by_currency(test,'USD')
+    for i in range(1):
+        print(next(q))
+
+    print(next(q))
+
 
 
 
---
+++ src/generators.py
@@ -0,0 +1,20 @@
+from typing import Union
+def filter_by_currency(tansactions_list:list[dict[any:any]], currency:str)->list[dict[any:any]]:
+    '''Возвращет итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной'''
+
+
+
+
+
+def transaction_descriptions(transactions):
+    '''принимает список словарей с транзакциями и возвращает по очереди описание каждой операции'''
+    pass
+
+
+def card_number_generator(start_data,end_data):
+    '''выдает номера банковских карт в аданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Принимает начальное и конечное значения для генерации диапазона номеров'''
+    pass
+
+
+
