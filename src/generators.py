fdff77825c6520f406326e80f8d3f198d8b9b737
aleksandr <kharitonov_am@bk.ru>
2024-11-20 20:52:53 +0300

    make isort, black, mypy, flake8 for test and src, no errors

--- src/generators.py
+++ src/generators.py
@@ -1,20 +1,32 @@
-from typing import Union
-def filter_by_currency(tansactions_list:list[dict[any:any]], currency:str)->list[dict[any:any]]:
-    '''Возвращет итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной'''
-
-    return filter(lambda transaction: transaction["operationAmount"]["currency"]["name"] == currency, tansactions_list)
+from typing import Any, Generator
 
 
+def filter_by_currency(
+    transactions_list: list[dict[Any, Any]], currency: str
+) -> filter[dict[Any, Any]]:
+    """Возвращет итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной"""
 
-def transaction_descriptions(transactions:list[dict[any:any]])->list[dict[any:any]]:
-    '''принимает список словарей с транзакциями и возвращает по очереди описание каждой операции'''
-    return (x['description'] for x in transactions)
+    return filter(
+        lambda transaction: transaction["operationAmount"]["currency"]["name"]
+        == currency,
+        transactions_list,
+    )
 
 
-def card_number_generator(start_data:int,end_data:int)->list[str]:
-    '''выдает номера банковских карт в аданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
-    Принимает начальное и конечное значения для генерации диапазона номеров'''
-    if start_data < 1 or end_data >9999999999999999 or start_data >= end_data:
-        raise ValueError('Введите корректные значения')
-    kard_number = ['0' * (16 - len(str(i))) + str(i) for i in range(start_data, end_data + 1)]
-    return [' '.join([item[index - 4:index] for index in range(4, 17, 4)]) for item in kard_number]
+def transaction_descriptions(transactions: list[dict[Any, Any]]) -> Generator[str]:
+    """принимает список словарей с транзакциями и возвращает по очереди описание каждой операции"""
+    return (x["description"] for x in transactions)
+
+
+def card_number_generator(start_data: int, end_data: int) -> list[str]:
+    """выдает номера банковских карт в аданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Принимает начальное и конечное значения для генерации диапазона номеров"""
+    if start_data < 1 or end_data > 9999999999999999 or start_data >= end_data:
+        raise ValueError("Введите корректные значения")
+    kard_number = [
+        "0" * (16 - len(str(i))) + str(i) for i in range(start_data, end_data + 1)
+    ]
+    return [
+        " ".join([item[index - 4 : index] for index in range(4, 17, 4)])
+        for item in kard_number
+    ]
--- test/conftest.py
+++ test/conftest.py
@@ -10,106 +10,97 @@ def make_rand_card_number():
 
 @pytest.fixture
 def make_generator_filter_list():
-    return [{
-          "id": 939719570,
-          "state": "EXECUTED",
-          "date": "2018-06-30T02:08:58.425572",
-          "operationAmount": {
-              "amount": "9824.07",
-              "currency": {
-                  "name": "USD",
-                  "code": "USD"
-              }
-          },
-          "description": "Перевод организации",
-          "from": "Счет 75106830613657916952",
-          "to": "Счет 11776614605963066702"
-      },
-      {
-              "id": 142264268,
-              "state": "EXECUTED",
-              "date": "2019-04-04T23:20:05.206878",
-              "operationAmount": {
-                  "amount": "79114.93",
-                  "currency": {
-                      "name": "USD",
-                      "code": "USD"
-                  }
-              },
-              "description": "Перевод со счета на счет",
-              "from": "Счет 19708645243227258542",
-              "to": "Счет 75651667383060284188"
-       }
-]
-
-@pytest.fixture
-def make_long_generator_filter_list():
-    return [{
-          "id": 939719570,
-          "state": "EXECUTED",
-          "date": "2018-06-30T02:08:58.425572",
-          "operationAmount": {
-              "amount": "9824.07",
-              "currency": {
-                  "name": "USD",
-                  "code": "USD"
-              }
-          },
-          "description": "Перевод организации",
-          "from": "Счет 75106830613657916952",
-          "to": "Счет 11776614605963066702"
-      },
-      {
-              "id": 142264268,
-              "state": "EXECUTED",
-              "date": "2019-04-04T23:20:05.206878",
-              "operationAmount": {
-                  "amount": "79114.93",
-                  "currency": {
-                      "name": "USD",
-                      "code": "USD"
-                  }
-              },
-              "description": "Перевод со счета на счет",
-              "from": "Счет 19708645243227258542",
-              "to": "Счет 75651667383060284188"
-       },
-       {
-            "id": 142264268,
+    return [
+        {
+            "id": 939719570,
             "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
+            "date": "2018-06-30T02:08:58.425572",
             "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "BYN",
-                    "code": "BYN"
-                }
+                "amount": "9824.07",
+                "currency": {"name": "USD", "code": "USD"},
             },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-       },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
         {
             "id": 142264268,
             "state": "EXECUTED",
             "date": "2019-04-04T23:20:05.206878",
             "operationAmount": {
                 "amount": "79114.93",
-                "currency": {
-                    "name": "RUS",
-                    "code": "RUS"
-                }
+                "currency": {"name": "USD", "code": "USD"},
             },
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        }
-]
+            "to": "Счет 75651667383060284188",
+        },
+    ]
+
+
+@pytest.fixture
+def make_long_generator_filter_list():
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {"name": "BYN", "code": "BYN"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {"name": "RUS", "code": "RUS"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+    ]
+
 
 @pytest.fixture
 def card_dannie_info():
-    return [[1,5], ['0000 0000 0000 0001',
-                    '0000 0000 0000 0002',
-                    '0000 0000 0000 0003',
-                    '0000 0000 0000 0004',
-                    '0000 0000 0000 0005']]
+    return [
+        [1, 5],
+        [
+            "0000 0000 0000 0001",
+            "0000 0000 0000 0002",
+            "0000 0000 0000 0003",
+            "0000 0000 0000 0004",
+            "0000 0000 0000 0005",
+        ],
+    ]
