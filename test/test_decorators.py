650063b7da125b6241cc122db568d7a58a5a1b39
aleksandr <kharitonov_am@bk.ru>
2024-11-26 22:55:37 +0300

    add test for log by capsys, cover is 100%

--- test/test_decorators.py
+++ test/test_decorators.py
@@ -6,7 +6,8 @@ def test_log_decorator():
     @log()
     def add_number(a,b):
         return a+b
-    z = add_number(5,7)
+
+    add_number(5,7)
     assert 'add_number ok'
 
 
@@ -14,6 +15,20 @@ def test_log_decorator_with_bad_data():
     @log()
     def divide_number(a,b):
         return a/b
+
     divide_number(5,0)
     assert 'divide_number error: division by zero. Inputs: (5, 0), {}'
+
+
+def test_capsys_log_decorator(capsys):
+    @log()
+    def minus_number(a,b,c,d):
+        result = a - b-c-d
+        print(result)
+        return result
+
+    minus_number(100, 50, 30, 20)
+    captured = capsys.readouterr()
+    assert captured.out == '0\nminus_number ok\n'
+
 
