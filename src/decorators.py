1dd051d529f26f9c4be6fac294038cb3e9342cc9
aleksandr <kharitonov_am@bk.ru>
2024-11-26 23:05:43 +0300

    flake8 done without mistakes for src and test

--- src/decorators.py
+++ src/decorators.py
@@ -1,35 +1,17 @@
-def log(filename = ''):
+def log(filename: str = ''):
     '''Автоматически логирует начало и конец выполнения функции,    ее результаты или возникшие ошибки, если
     filename задан, логи записываются в указанный файл, если не задан, логи выводятся в консоль'''
     def decorator(func):
-        def wrapper(*args,**kwargs):
+        def wrapper(*args, **kwargs):
             try:
                 func(*args, **kwargs)
                 result = f'{func.__name__} ok'
             except Exception as e:
                 result = f'{func.__name__} error: {e}. Inputs: {args}, {kwargs}'
             if filename:
-                with open(filename,'a') as f:
-                    f.write(result+'\n')
+                with open(filename, 'a') as f:
+                    f.write(result + '\n')
             else:
                 print(result)
-            #return func(*args, **kwargs)
         return wrapper
     return decorator
-
-
-@log('test_log.txt')
-def sum(a,b):
-    return a+b
-
-z = sum(5,8)
-
-
-@log('bad_test.txt')
-def dived(a,b):
-    return a/b
-
-l = dived(5,1)
-
-l = dived(5,0)
-
--- test/test_decorators.py
+++ test/test_decorators.py
@@ -1,34 +1,33 @@
-import pytest
+from _pytest.capture import CaptureFixture
+
 from src.decorators import log
 
 
-def test_log_decorator():
+def test_log_decorator() -> None:
     @log()
-    def add_number(a,b):
-        return a+b
+    def add_number(a, b):
+        return a + b
 
-    add_number(5,7)
+    add_number(5, 7)
     assert 'add_number ok'
 
 
-def test_log_decorator_with_bad_data():
+def test_log_decorator_with_bad_data() -> None:
     @log()
-    def divide_number(a,b):
-        return a/b
+    def divide_number(a, b):
+        return a / b
 
-    divide_number(5,0)
+    divide_number(5, 0)
     assert 'divide_number error: division by zero. Inputs: (5, 0), {}'
 
 
-def test_capsys_log_decorator(capsys):
+def test_capsys_log_decorator(capsys: CaptureFixture[str]) -> None:
     @log()
-    def minus_number(a,b,c,d):
-        result = a - b-c-d
+    def minus_number(a, b, c, d):
+        result = a - b - c - d
         print(result)
         return result
 
     minus_number(100, 50, 30, 20)
     captured = capsys.readouterr()
     assert captured.out == '0\nminus_number ok\n'
-
-
