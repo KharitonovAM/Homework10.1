0cd6b66214cf3bd695f91fcdeee3d068299ec09b
aleksandr <kharitonov_am@bk.ru>
2024-12-03 21:32:45 +0300

    change get_exchange

--- src/external_api.py
+++ src/external_api.py
@@ -1,20 +1,19 @@
 import json
+import os
+from dotenv import load_dotenv
 import requests
-
-with open('../.env') as f:
-    my_api = json.load(f)['ApiKey']
 
 def get_exchange(amount, frm):
     '''Принимает на вход сумму и валюту, возвращает сумму в рублях'''
 
+    load_dotenv()
+    my_api = os.getenv('ApiKey')
     to = "RUB"
     url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={frm}&amount={amount}"
     payload = {}
     headers = {
         "apikey": my_api
     }
-
     response = requests.request("GET", url, headers=headers, data=payload)
     repos = response.json()
-    print(repos)
     return repos['result']
---
+++ test/test_external_api.py
@@ -0,0 +1,4 @@
+import pytest
+from src.external_api import get_exchange
+
+def
