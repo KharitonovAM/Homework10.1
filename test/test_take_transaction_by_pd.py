a339924e9ec1c1f0b9d343b67e275bc68e4ee868
aleksandr <kharitonov_am@bk.ru>
2024-12-10 23:23:55 +0300

    Добавил тесты для проверки получения данных из exel

--- test/test_take_transaction_by_pd.py
+++ test/test_take_transaction_by_pd.py
@@ -1,4 +1,5 @@
 import pytest
+import pandas as pd
 import os
 from unittest.mock import Mock, patch
 from src.take_transaction_by_pb import take_transactions_from_exel, take_transactions_from_csv
@@ -15,3 +16,17 @@ def test_take_transactions_from_csv(mock_csv_DictReader):
 def test_with_wrong_file_csv():
     with pytest.raises(FileNotFoundError):
         raise take_transactions_from_csv('bad filename')
+
+
+def test_with_wrong_file_exel():
+    with pytest.raises(FileNotFoundError):
+        raise take_transactions_from_exel('bad filename')
+
+
+
+def test_patch_test_take_transactions_from_exel():
+    z = [{'name': 'Ivan', 'family': 'Бунин'}, {'name': 'Olga', 'family': 'Кортункова'}]
+    my_datafraim = pd.DataFrame(z)
+    my_datafraim.to_excel('my_exel.xlsx', index=False)
+    assert take_transactions_from_exel('my_exel.xlsx') == [{'name': 'Ivan', 'family': 'Бунин'}, {'name': 'Olga', 'family': 'Кортункова'}]
+    os.remove('my_exel.xlsx')
