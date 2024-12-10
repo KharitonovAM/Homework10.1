94465825dfb1ad5405da63c5e163e1e952acbc67
aleksandr <kharitonov_am@bk.ru>
2024-12-10 18:20:10 +0300

    Добавил функцию по получению трансакции из эксель

--- src/take_transaction_by_pb.py
+++ src/take_transaction_by_pb.py
@@ -9,3 +9,9 @@ def take_transactions_from_csv(filename):
         data = csv.DictReader(f, delimiter=';')
         data_list = [row for row in data]
     return data_list
+
+
+def take_transactions_from_exel(filename):
+    '''Функция для считывания финансовых операций из EXEL выдает список словарей с транзакциями'''
+    exel_data = pd.read_excel(filename)
+    return exel_data.to_dict('records')
