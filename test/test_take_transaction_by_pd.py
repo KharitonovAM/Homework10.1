b68b3d0e137a266d2e5cba178a21f14d32b27ee0
aleksandr <kharitonov_am@bk.ru>
2024-12-10 18:30:36 +0300

    Добавил тест для функции по получениию данных из csv

--- test/test_take_transaction_by_pd.py
+++ test/test_take_transaction_by_pd.py
@@ -1,3 +1,12 @@
 import pytest
+import os
 from unittest.mock import Mock, patch
 from src.take_transaction_by_pb import take_transactions_from_exel, take_transactions_from_csv
+
+
+@patch('csv.DictReader', return_value = [{'data':'123', 'name':'MyName'}, {'data':121, 'name':'Alice'}])
+def test_take_transactions_from_csv(mock_csv_DictReader):
+    open('test.csv','w')
+    assert take_transactions_from_csv('test.csv') == [{'data':'123', 'name':'MyName'}, {'data':121, 'name':'Alice'}]
+    mock_csv_DictReader.assert_called_once()
+    os.remove('test.csv')
