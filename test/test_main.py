84619142b3dd88c73e59b1779361e9f780d25bf5
aleksandr <kharitonov_am@bk.ru>
2024-12-17 20:16:57 +0300

    Увеличел покрытие тестами функции main до 83% покрытие тестами

--- test/test_main.py
+++ test/test_main.py
@@ -6,3 +6,7 @@ from main import main
 @patch('main.input', side_effect=['1', 'executed', 'да', 'по возрастанию', "нет", "да", "Перевод"])
 def test_main(mock_input):
     main()
+
+@patch('main.input', side_effect=['1', 'таркан', 'executed', 'да', 'по возрастанию', "нет", "нет"])
+def test_main(mock_input):
+    main()
